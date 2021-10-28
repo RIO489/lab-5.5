6e51f731972d4e81b0a083e214c21f40c01f56f5
RIO489 <oleksander.hamaiunov.it.2021@lpnu.ua>
2021-10-28 09:58:32 +0300

    lab-5.5.1

---
+++ lab-5.5.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+int Q(const int m, const int n, int level, int& depth)
+{
+    if (level > depth)
+        depth = level;
+
+    if (m == 1 || n == 1)
+    {
+        return  1;
+    }
+    else
+        if (m < n)
+        {
+            return  Q(n, m, level + 1, depth);
+        }
+        else
+            if (m == n)
+            {
+                return  1 - Q(m, m - 1, level + 1, depth);
+            }
+            else
+                return  Q(m, n - 1, level + 1, depth) + Q(m - n, n, level + 1, depth);
+
+}
+
+
+int main()
+{
+    int m, n, depth;
+    cout << "m = "; cin >> m;
+    cout << "n = "; cin >> n;
+    cout << "Q =" << Q(m, n, 1, depth) << endl;
+    cout << "depth =" << depth << endl;
+
+    return 0;
+}
+
+
+
+
